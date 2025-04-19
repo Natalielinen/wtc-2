dea58ce24077723ac945cc86245c5f582787b46f
Natalie Chufistova <nataliech0409@gmail.com>
2025-04-19 07:06:40 +0300

    Отображение кнопок для входа на главной странице, если пользователь не авторизован

--- app/shared/itemsList.tsx
+++ app/shared/itemsList.tsx
@@ -2,18 +2,27 @@
 
 import { Preview } from "./preview"
 import { useUserStore } from "../stores/userStore";
+import { Button } from "@/components/ui/button";
+import React from "react";
 
 export const ItemsList = () => {
     // Данные и состояние
-    const user = useUserStore((state) => state.currentUser);
+    const { currentUser: user, setOpenLoginModal } = useUserStore((state) => state);
+
+    const openLoginModal = () => setOpenLoginModal(true);
 
     // UI
-    return <div className="grid grid-cols-3 gap-4 sm:grid-cols-4">
+    return user ? <div className="grid grid-cols-3 gap-4 sm:grid-cols-4">
         {
-            user ? user.userItems.map((item) => (
+            user.userItems.map((item) => (
                 <Preview key={item.id} item={item} />
-            )) : "Загрузка..."
+            ))
         }
 
-    </div>
+    </div> : <p>
+        <Button variant="link" onClick={openLoginModal}>Войдите</Button>
+        или
+        <Button variant="link" onClick={openLoginModal}>Зарегистрируйтесь</Button>
+        чтобы добавлять и просматривать фильмы и игры
+    </p>
 }
--- app/stores/userStore.ts
+++ app/stores/userStore.ts
@@ -4,12 +4,16 @@ import { User } from '../types';
 
 interface UserStore {
     currentUser: User | null;
+    openLoginModal: boolean;
     setCurrentUser: (user: User | null) => void;
-    logout: () => void
+    logout: () => void;
+    setOpenLoginModal: (open: boolean) => void;
 }
 
 export const useUserStore = create<UserStore>((set) => ({
   currentUser: null,
+  openLoginModal: false,
   setCurrentUser: (user: User | null) => set({ currentUser: user }),
   logout: () => set({ currentUser: null }),
+  setOpenLoginModal: (open: boolean) => set({ openLoginModal: open }),
 }))
